5a1c24b233b53e9ffa872eda3032b8b1cbab3949
ReverseOfCode <patteon@naver.com>
2025-09-29 20:30:46 +0900

    pakage 재작성

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-  ],
+  // Tailwind CSS v4: PostCSS 플러그인은 '@tailwindcss/postcss'만 필요합니다.
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
